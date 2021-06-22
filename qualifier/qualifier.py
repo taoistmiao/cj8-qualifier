a6ce9baf149ffea683e41a6d8ab3102e96d59573
miaog <gtmiao99@gmail.com>
2021-06-22 00:52:15 -0400

    add some test

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -86,5 +86,33 @@ print(make_table(
 ))
 
 print(make_table(
-    
+    rows=[
+        ["Ducky Yellow", 3],
+        ["Ducky Dave", 12],
+        ["Ducky Tube", 7],
+        ["Ducky Lemon", 1]
+    ],
+   labels=["Name", "Duckiness"],
+   centered=True
+))
+
+print(make_table(
+    rows=[
+        ["Lemon", 18_3285, "Owner"],
+        ["Sebastiaan", 18_3285.1, "Owner"],
+        ["KutieKatj", 15_000, "Admin"],
+        ["Jake", "MoreThanU", "Helper"],
+        ["Joe", -12, "Idk Tbh"]
+    ],
+    labels=["User", "Messages", "Role"]
+))
+
+print(make_table(
+    rows=[
+        ["Lemon"],
+        ["Sebastiaan"],
+        ["KutieKatj9"],
+        ["Jake"],
+        ["Not Joe"]
+    ]
 ))
